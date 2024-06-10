af6e85621118f3f241906189096e91c5aea89861
Matheus Cavalcante <jmatheusoc269@gmail.com>
2024-06-10 20:48:24 -0300

    new exercise

---
+++ Questions/ex9.rb
@@ -0,0 +1,12 @@
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts %q{
+There's somthing going on here.
+With this weird quote
+i think this mean that here we can write big texts
+ }
