36757f2f76efeb572ebc8d548914bb556886c869
Matheus Cavalcante <jmatheusoc269@gmail.com>
2024-06-10 15:54:39 -0300

    newExercise

---
+++ Questions/ex4.rb
@@ -0,0 +1,17 @@
+
+cars = 100
+people = 60
+
+puts "there are #{cars} available"
+puts "And more #{people} will join"
+
+def greeting()
+  puts "Hi people"
+end
+
+puts " Ola pessoas #{greeting()}"
+
+car_in_river = 10
+cars_in_space = 10
+
+puts "Are more #{car_in_river + cars_in_space} cars than #{people} people"
