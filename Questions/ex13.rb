dc0af6097a0141cd1a77cc82ae9776134d59be40
Matheus Cavalcante <jmatheusoc269@gmail.com>
2024-06-13 01:25:18 -0300

    just some ajusts

--- Questions/ex13.rb
+++ Questions/ex13.rb
@@ -1,6 +1,15 @@
 
 first, second, third = ARGV
 
+
+puts "Give me a first number:"
+first = gets.chomp
+puts "Give me a second number:"
+second = gets.chomp
+puts "Give me a third number:"
+third = gets.chomp
+
 puts "Your first variable is: #{first}"
+
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
